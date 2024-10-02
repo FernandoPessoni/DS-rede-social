9091fa95cd5334bacfac010bae46f24a597f797f
FernandoPessoni <104445764+FernandoPessoni@users.noreply.github.com>
2024-10-02 08:20:27 -0300

    ok

--- graficos/quantidadedeUsuarios.js
+++ graficos/quantidadedeUsuarios.js
@@ -1,15 +1,14 @@
 import { getCSS } from "./common.js";
-
 async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
-  
+  const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+  const res = await fetch(url)
+  const dados = await res.json()
+  const nomeDasRedes = Object.keys(dados)
+  const quantidadeDeUsuarios = Object.values(dados)
+
   const data = [
     {
-      x: 'nomeDasRedes',
+      x: nomeDasRedes,
       y: quantidadeDeUsuarios,
       type: 'bar',
       marker: {
@@ -21,12 +20,13 @@ async function quantidadeUsuarios() {
   const layout = {
     plot_bgcolor: getCSS('--bg-color'),
     paper_bgcolor: getCSS('--bg-color')
-  }
+}
 
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, layout)
+const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data, layout)
+
 }
 
 quantidadeUsuarios()
