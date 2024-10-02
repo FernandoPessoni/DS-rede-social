f6b19e14f0e2f6f8a6f522d0981f0956a254c754
FernandoPessoni <104445764+FernandoPessoni@users.noreply.github.com>
2024-10-02 08:11:40 -0300

    ok

--- graficos/quantidadedeUsuarios.js
+++ graficos/quantidadedeUsuarios.js
@@ -1,15 +1,12 @@
 import { getCSS } from "./common.js";
 
 async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
     const quantidadeUsuarios = Object.values(dados)
   
-  
-  
-
   const data = [
     {
       x: 'nomeDasRedes',
